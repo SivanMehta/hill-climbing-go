61267d742d1bd1a936c6ab2485b24faae07687be
Sivan Mehta <Sivanmehta2@gmail.com>
2018-04-30 18:01:25 -0700

    :tada: it works

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-Trying to implement [hill climbing](https://en.wikipedia.org/wiki/Hill_climbing) in `go`. Run `make`:
+Trying to implement [hill climbing](https://en.wikipedia.org/wiki/Hill_climbing) to estimate the maximum value between x = [0, 10] for the following function in `go`:
+
+```
+y = sin(x)
+```
+
+The correct answers(s) is are (π / 2, 1) and/or (5π / 2, 1)
+
+
+Run `make` to run the optimization:
 
 ```sh
 make
-
 ``
--- hill.go
+++ hill.go
@@ -2,8 +2,38 @@ package main
 
 import (
   "log"
+  "math"
+  "math/rand"
 )
 
+const (
+  workers int = 15
+  max float64 = 10
+  stepSize float64 = .001
+  down float64 = -.001
+)
+
+func climb() float64 {
+  seed := rand.Float64() * max
+  right := seed + stepSize
+  left := seed - stepSize
+
+  for math.Sin(seed) < math.Sin(right) || math.Sin(seed) < math.Sin(left) {
+    if(math.Sin(seed) < math.Sin(right)) { // climb to the right
+      seed = right
+    } else { // climb to the left
+      seed = left
+    }
+    right = seed + stepSize
+    left = seed - stepSize
+  }
+
+  return seed
+}
+
 func main()  {
-  log.Println("hello");
+  for i := 0; i < 10; i++ {
+    maximum := climb()
+    log.Println(maximum / math.Pi)
+  }
 }
